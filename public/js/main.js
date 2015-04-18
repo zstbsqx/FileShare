1c57d1ea434c31f5997b843fc339a7237660454b
程季 <zstbcj@gmail.com>
2015-04-19 00:16:10 +0800

    add detail button, improve dynamic list

--- public/js/main.js
+++ public/js/main.js
@@ -5,8 +5,8 @@ function $id(id) {
     return document.getElementById(id);
 }
 
-function $class(class) {
-    return document.getElementsByClassName(class);
+function $byclass(byclass) {
+    return document.getElementsByClassName(byclass);
 }
 
 function $all(query) {
@@ -17,21 +17,90 @@ function $(query) {
     return document.querySelector(query);
 }
 
+var reader;
+var progress = document.querySelector('.percent');
+
+function abortRead() {
+    reader.abort();
+}
+
+function errorHandler(evt) {
+switch(evt.target.error.code) {
+    case evt.target.error.NOT_FOUND_ERR:
+        alert('File Not Found!');
+        break;
+    case evt.target.error.NOT_READABLE_ERR:
+        alert('File is not readable');
+        break;
+    case evt.target.error.ABORT_ERR:
+        break; // noop
+    default:
+        alert('An error occurred reading this file.');
+};
+}
+
+function updateProgress(evt) {
+    // evt is an ProgressEvent.
+    if (evt.lengthComputable) {
+      var percentLoaded = Math.round((evt.loaded / evt.total) * 100);
+      // Increase the progress bar length.
+      if (percentLoaded < 100) {
+        progress.style.width = percentLoaded + '%';
+        progress.textContent = percentLoaded + '%';
+      }
+    }
+}
+
 var dropbox = $id('dropbox');
-var files = $id('files');
+var filelist = $id('files');
+
+function addItems(files) {
+    //clear filelist
+    while(filelist.lastChild) {
+        filelist.removeChild(filelist.lastChild);
+    }
+    var len = files.length;
+    for (var i = 0; i < len; i++) {
+        var li = document.createElement('li');
+        var fileName = document.createElement('div');
+        fileName.innerHTML = files[i].name;
+        fileName.className = 'fileName';
+        var detailBtn = document.createElement('button');
+        detailBtn.innerHTML = 'More details(click to show)';
+        detailBtn.addEventListener('click', function(ev) {
+            next = detailBtn.nextSibling;
+            if(next.style.display === 'none') {
+                detailBtn.innerHTML = 'More details(click to hide)';
+                next.style.display = 'block';
+            } else {
+                detailBtn.innerHTML = 'More details(click to show)';
+                next.style.display = 'none';
+            }
+        });
+        var fileDetail = document.createElement('div');
+        fileDetail.style.display = 'none';
+        var fileType = document.createElement('div');
+        fileType.innerHTML = (files[i].type?files[i].type:'unknown type');
+        fileType.className = 'fileType';
+        var fileSize = document.createElement('div');
+        fileSize.innerHTML = files[i].size + ' bytes';
+        fileSize.className = 'fileSize';
+        li.appendChild(fileName);
+        li.appendChild(detailBtn);
+        li.appendChild(fileDetail);
+        fileDetail.appendChild(fileType);
+        fileDetail.appendChild(fileSize);
+        filelist.appendChild(li);
+    }
+}
+
+
+
 dropbox.addEventListener('drop', function (e) {
     e.stopPropagation();
     e.preventDefault();
     dropbox.className = null;
-    files.innerHTML = "";
-    var len = e.dataTransfer.files.length;
-    for (var i = 0; i < len; i++) {
-        var li = document.createElement('li');
-        var str = [e.dataTransfer.files[i].name, '(' + (e.dataTransfer.files[i].type?e.dataTransfer.files[i].type:'unknown type') + ')',  e.dataTransfer.files[i].size + ' bytes', 'lastModifiedDate: ' + e.dataTransfer.files[i].lastModifiedDate,].join('<br>');
-        console.log(str);
-        li.innerHTML = str;
-        files.appendChild(li);
-    }
+    addItems(e.dataTransfer.files);
 });
 
 dropbox.addEventListener('dragenter', function (e) {
@@ -46,6 +115,6 @@ dropbox.addEventListener('dragleave', function (e) {
     dropbox.className = null;
 });
 
-function D(e) {
+dropbox.addEventListener('dragover', function (e) {
     e.preventDefault();
-}
+});
