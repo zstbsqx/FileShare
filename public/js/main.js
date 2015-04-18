34306785767baf5d2569a746452a567099ca5268
程季 <zstbcj@gmail.com>
2015-04-19 01:13:00 +0800

    finish progressbar & add fileReader

--- public/js/main.js
+++ public/js/main.js
@@ -25,34 +25,61 @@ function abortRead() {
 }
 
 function errorHandler(evt) {
-switch(evt.target.error.code) {
-    case evt.target.error.NOT_FOUND_ERR:
-        alert('File Not Found!');
-        break;
-    case evt.target.error.NOT_READABLE_ERR:
-        alert('File is not readable');
-        break;
-    case evt.target.error.ABORT_ERR:
-        break; // noop
-    default:
-        alert('An error occurred reading this file.');
-};
+    switch(evt.target.error.code) {
+        case evt.target.error.NOT_FOUND_ERR:
+            alert('File Not Found!');
+            break;
+        case evt.target.error.NOT_READABLE_ERR:
+            alert('File is not readable');
+            break;
+        case evt.target.error.ABORT_ERR:
+            break; // noop
+        default:
+            alert('An error occurred reading this file.');
+    };
 }
 
 function updateProgress(evt) {
     // evt is an ProgressEvent.
     if (evt.lengthComputable) {
-      var percentLoaded = Math.round((evt.loaded / evt.total) * 100);
-      // Increase the progress bar length.
-      if (percentLoaded < 100) {
-        progress.style.width = percentLoaded + '%';
-        progress.textContent = percentLoaded + '%';
-      }
+        var percentLoaded = Math.round((evt.loaded / evt.total) * 100);
+        // Increase the progress bar length.
+        if (percentLoaded < 100) {
+            progress.style.width = percentLoaded + '%';
+            progress.textContent = percentLoaded + '%';
+        }
     }
 }
 
 var dropbox = $id('dropbox');
 var filelist = $id('files');
+
+function startProgress(file, progressBar, percentageBar) {
+    console.log('start progress');
+    var reader = new FileReader();
+    reader.onerror = errorHandler;
+    reader.onabort = function (ev) {
+        alert('File read aborted.');
+    }
+    reader.onprogress = function (ev) {
+        if(ev.lengthComputable) {
+            var percentage = (ev.loaded/ev.total)*100;
+            if(percentage < 100) {
+                percentageBar.style.width = percentage + '%';
+                percentageBar.innerHTML = percentage + '%';
+            }
+            console.log('%f percentage uploaded', percentage);
+        }
+    }
+    reader.onload = function (ev) {
+        percentageBar.style.width = '100%';
+        percentageBar.innerHTML = '100%';
+        //transfer file
+    }
+    //if file is a text file
+    reader.readAsText(file);
+    //else readAsBinaryString
+}
 
 function addItems(files) {
     //clear filelist
@@ -65,6 +92,13 @@ function addItems(files) {
         var fileName = document.createElement('div');
         fileName.innerHTML = files[i].name;
         fileName.className = 'fileName';
+        var progressBar = document.createElement('div');
+        progressBar.className = 'progressbar';
+        var percentageBar = document.createElement('div');
+        percentageBar.className = 'percentagebar';
+        percentageBar.innerHTML = '0%';
+        percentageBar.style.width = '0%';
+        progressBar.appendChild(percentageBar);
         var detailBtn = document.createElement('button');
         detailBtn.innerHTML = 'More details(click to show)';
         detailBtn.addEventListener('click', function(ev) {
@@ -80,17 +114,19 @@ function addItems(files) {
         var fileDetail = document.createElement('div');
         fileDetail.style.display = 'none';
         var fileType = document.createElement('div');
-        fileType.innerHTML = (files[i].type?files[i].type:'unknown type');
+        fileType.innerHTML = ('Type' + files[i].type?files[i].type:'unknown type');
         fileType.className = 'fileType';
         var fileSize = document.createElement('div');
-        fileSize.innerHTML = files[i].size + ' bytes';
+        fileSize.innerHTML = 'Type' + files[i].size + ' bytes';
         fileSize.className = 'fileSize';
         li.appendChild(fileName);
+        li.appendChild(progressBar);
         li.appendChild(detailBtn);
         li.appendChild(fileDetail);
         fileDetail.appendChild(fileType);
         fileDetail.appendChild(fileSize);
         filelist.appendChild(li);
+        startProgress(files[i], progressBar, percentageBar);
     }
 }
 
